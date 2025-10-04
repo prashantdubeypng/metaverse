c97931e80b5ba4ed0b1d35ac1c7facb032fed0b9
prashant dubey <143105656+prashantdubeypng@users.noreply.github.com>
2025-10-05 01:45:09 +0530

    Update API URLs to use environment variable

--- frontend/src/app/create-space/page.tsx
+++ frontend/src/app/create-space/page.tsx
@@ -41,7 +41,7 @@ export default function CreateSpacePage() {
       }
 
       const response = await fetch(
-        "http://localhost:8000/api/v1/user/space/maps/refernce",
+        "process.env.NEXT_PUBLIC_API_URL/user/space/maps/refernce",
         {
           method: "GET",
           headers: {
@@ -111,7 +111,7 @@ export default function CreateSpacePage() {
       }
 console.log("tokenData in CreateSpacePage:", tokenData);
 console.log('i am sending the data to the route')
-      const response = await fetch("http://localhost:8000/api/v1/space/", {
+      const response = await fetch("process.env.NEXT_PUBLIC_API_URL/space/", {
         method: "POST",
         headers: {
           'Authorization': `Bearer ${tokenData.token}`,
