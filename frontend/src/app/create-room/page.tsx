0a4649ad6dffb933fb85217715f8a88f323d37ee
prashant dubey <143105656+prashantdubeypng@users.noreply.github.com>
2025-10-05 02:03:25 +0530

    Update API URLs to use direct endpoint

--- frontend/src/app/create-room/page.tsx
+++ frontend/src/app/create-room/page.tsx
@@ -36,7 +36,7 @@ export default function CreateSpacePage() {
         return;
       }
       const response = await fetch(
-        "process.env.NEXT_PUBLIC_API_URL/user/space/maps/refernce",
+        "https://metaverse-http-u3ys.onrender.com/api/v1/user/space/maps/refernce",
         {
           method: "GET",
           headers: {
@@ -103,7 +103,7 @@ export default function CreateSpacePage() {
           dimensions: `${customWidth}x${customHeight}`,
         };
       }
-      const response = await fetch("process.env.NEXT_PUBLIC_API_URL/space/", {
+      const response = await fetch("https://metaverse-http-u3ys.onrender.com/api/v1/space/", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${tokenData.token}`,
